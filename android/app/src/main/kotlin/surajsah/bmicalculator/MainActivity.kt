8c3fd11dfff03ee0ce2392f7d66e3caea2d8836e
surrajj20 <shah.suraj1200@gmail.com>
2020-07-10 09:24:03 +0530

    app-setup & theme-changed

---
+++ android/app/src/main/kotlin/surajsah/bmicalculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package surajsah.bmicalculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF10163B),
+        scaffoldBackgroundColor: Color(0xFF0C1234),
+      ),
+      home: InputPage(),
+    );
+  }
+}
