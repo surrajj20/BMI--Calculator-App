59d8761fb71a7e3fc43da9cd2051e6cc6d1b99be
surrajj20 <shah.suraj1200@gmail.com>
2020-07-11 13:16:31 +0530

    ternary operator use instade of if/else

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'input_page.dart';
 
-void main() {
-  runApp(BMICalculator());
-}
+void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
