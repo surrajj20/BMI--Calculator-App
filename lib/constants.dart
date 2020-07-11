0cb574507f42cc6d1a99900a594de95a4e59b27f
surrajj20 <shah.suraj1200@gmail.com>
2020-07-11 15:03:05 +0530

    height & slider added/ constants seperated

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+//const activeCardColour = Color(0xFF272A4E);
+const kActiveCardColour = Color(0xFF141A3C);
+const kInactiveCardColour = Color(0xFF111328);
+const kBottomContainerColour = Color(0xFFFF0067);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
